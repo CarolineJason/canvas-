cc03aaeb99544a347da78eb19be60e42ad96cb5f
iyb-huanglin <huanglin@iyunbao.com>
2019-11-20 10:33:14 +0800

    fix

--- canvas-train/src/App.js
+++ canvas-train/src/App.js
@@ -1,27 +1,29 @@
 import React from 'react';
 
-// import CanvasApi from './CanvasApi';
-// import Qiqiaoban from './Qiqiaoban';
-// import DrawArc from './DrawArc';
-// import SaveStore from './saveRestore';
-// import Translating from './Translating';
-// import Rotating from './Rotating';
-// import Sacling from './Scaling';
-// import Clip from './Clip';
-import CanvasAnimation from './CanvasAnimation';
+// import CanvasApi from '@components/CanvasApi';
+// import Qiqiaoban from '@components/Qiqiaoban';
+// import DrawArc from '@components/DrawArc';
+// import SaveStore from '@components/saveRestore';
+// import Translating from '@components/Translating';
+// import Rotating from '@components/Rotating';
+// import Sacling from '@components/Scaling';
+// import Clip from '@components/Clip';
+// import CanvasAnimation from '@components/CanvasAnimation';
+import Clock from '@components/Clock';
 
 function App() {
   return (
     <div className="App">
-      {/* <CanvasApi /> */}
-      {/* <Qiqiaoban /> */}
-      {/* <DrawArc /> */}
-      {/* <SaveStore /> */}
-      {/* <Translating /> */}
-      {/* <Rotating /> */}
-      {/* <Sacling /> */}
-      {/* <Clip /> */}
-      <CanvasAnimation />
+      {/* <CanvasApi />
+      <Qiqiaoban />
+      <DrawArc />
+      <SaveStore />
+      <Translating />
+      <Rotating />
+      <Sacling /> */}
+      <Clip />
+      {/* <CanvasAnimation />
+      <Clock /> */}
     </div>
   );
 }
