961748b8613b0030511e37e1897321a9b3ab0576
iyb-huanglin <huanglin@iyunbao.com>
2019-11-26 10:54:55 +0800

    async await

--- canvas-train/src/App.js
+++ canvas-train/src/App.js
@@ -1,42 +1,49 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-// import CanvasApi from '@components/CanvasApi';
-// import Qiqiaoban from '@components/Qiqiaoban';
-// import DrawArc from '@components/DrawArc';
-// import SaveStore from '@components/saveRestore';
-// import Translating from '@components/Translating';
-// import Rotating from '@components/Rotating';
-// import Sacling from '@components/Scaling';
-// import Clip from '@components/Clip';
-// import CanvasAnimation from '@components/CanvasAnimation';
-// import Clock from '@components/Clock';
-// import DigitClock from './Clock/Clock';
-// import BallDown from './ballDown';
-// import Progress from '@components/Progress';
-// import Star from '@components/Star';
-// import SaveStore from '@components/SaveStore';
-// import CreateBackgroundCanvas from '@components/createBackgroundCanvas';
+import CanvasApi from '@components/CanvasApi';
+import Qiqiaoban from '@components/Qiqiaoban';
+import DrawArc from '@components/DrawArc';
+import SaveStore from '@components/saveRestore';
+import Translating from '@components/Translating';
+import Rotating from '@components/Rotating';
+import Sacling from '@components/Scaling';
+import Clip from '@components/Clip';
+import CanvasAnimation from '@components/CanvasAnimation';
+import Clock from '@components/Clock';
+import DigitClock from './Clock/Clock';
+import Progress from '@components/Progress';
+import Star from '@components/Star';
+import SaveStores from '@components/SaveStore';
+import CreateBackgroundCanvas from '@components/createBackgroundCanvas';
 import Moon from '@components/Moon';
+import ZhiMaScore from '@components/zhimaScore';
+import ProgressTrain from './Clock/progress-train';
 
 function App() {
   return (
     <div className="App">
-      {/* <CanvasApi /> */}
-      {/* <Qiqiaoban /> */}
-      {/* <DrawArc /> */}
-      {/* <SaveStore /> */}
-      {/* <Translating /> */}
-      {/* <Rotating /> */}
-      {/* <Sacling /> */}
-      {/* <Clip /> */}
-      {/* <CanvasAnimation /> */}
-      {/* <Clock /> */}
-      {/* <DigitClock /> */}
-      {/* <Progress /> */}
-      {/* <Star /> */}
-      {/* <SaveStore /> */}
-      {/* <CreateBackgroundCanvas /> */}
-      <Moon />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/canvasApi" component={CanvasApi}></Route>
+          <Route path="/qiqiaoban" component={Qiqiaoban}></Route>
+          <Route path="/arc" component={DrawArc}></Route>
+          <Route path="/saveStore1" component={SaveStore}></Route>
+          <Route path="/translating" component={Translating}></Route>
+          <Route path="/sacling" component={Sacling}></Route>
+          <Route path="/clip" component={Clip}></Route>
+          <Route path="/canvasAnimation" component={CanvasAnimation}></Route>
+          <Route path="/clock" component={Clock}></Route>
+          <Route path="/digitClock" component={DigitClock}></Route>
+          <Route path="/progress" component={Progress}></Route>
+          <Route path="/star" component={Star}></Route>
+          <Route path="/saveStore2" component={SaveStores}></Route>
+          <Route path="/createBackgroundCanvas" component={CreateBackgroundCanvas}></Route>
+          <Route path="/moon" component={Moon}></Route>
+          <Route path="/zhiMaScore" component={ZhiMaScore}></Route>
+          <Route path="/progressTrain" component={ProgressTrain}></Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
